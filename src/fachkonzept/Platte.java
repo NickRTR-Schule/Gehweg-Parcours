2366d75326c6b705356781fdd7c21a174f0e1114
luk4xsx <LukasO1804@web.de>
2022-05-18 14:33:58 +0200

    Methoden skizziert

--- src/fachkonzept/Platte.java
+++ src/fachkonzept/Platte.java
@@ -1,5 +1,26 @@
 package fachkonzept;
 
-public class Platte {
-
+public class Platte
+{
+	public boolean istSpielerAufPlatte()
+	{
+		
+	}
+	
+	public String liesPositionAufPlatte()
+	{
+		
+	}
+	
+	public void setzeHundehaufen()
+	{
+		
+	}
+	
+	public boolean istHundehaufenAufPlatte()
+	{
+		
+	}
+	
+	
 }
--- src/fachkonzept/Spieler.java
+++ src/fachkonzept/Spieler.java
@@ -1,5 +1,50 @@
 package fachkonzept;
 
-public class Spieler {
+public class Spieler
+{
+	public void vermutungSetzen()
+	{
+		
+	}
+	
+	public void laufeRechts()
+	{
+		
+	}
+	
+	public void laufeLinks()
+	{
+		
+	}
+	
+	public void laufeOben()
+	{
+		
+	}
+	
+	public void laufeUnten()
+	{
+		
+	}
+	
+	public void laufeNordOst()
+	{
+		
+	}
+	
+	public void laufeSüdOst()
+	{
+		
+	}
+	
+	public void laufeSüdWest()
+	{
+		
+	}
+	
+	public void laufeNordWest()
+	{
+		
+	}
 
 }
